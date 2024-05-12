f624595ae24088efc185aa0915ac1cc6becef27e
ayanshuDS <73418468+MET4L-DS@users.noreply.github.com>
2024-05-13 00:48:48 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div className="flex flex-col gap-10 overflow-x-hidden">
+            <section className="grid grid-cols-3 gap-10 p-10">
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+                <div className="flex aspect-square items-center justify-center gap-10 rounded-lg bg-slate-800"></div>
+            </section>
+        </div>
+    );
+}
+
+export default App;
